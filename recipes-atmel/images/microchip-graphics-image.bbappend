41ac00aabedac9978b30db6d9c0a71cf969df36e
zivku <marion01992@gmail.com>
2025-09-03 01:29:16 -0700

    holystone layer-v0.0.1

--- recipes-atmel/images/microchip-graphics-image.bbappend
+++ recipes-atmel/images/microchip-graphics-image.bbappend
@@ -1,1 +1,2 @@
 IMAGE_INSTALL:append = " sqlite3 libsqlite3"
+IMAGE_INSTALL:append = " tzdata"
--- recipes-myapps/wifi-auto/files/wifi-auto.sh
+++ recipes-myapps/wifi-auto/files/wifi-auto.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+ln -sf /usr/share/zoneinfo/Asia/Taipei /etc/localtime
 echo "This is Holystone demo" | tee /dev/ttyS0
